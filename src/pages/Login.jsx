e5b5550066ead95e89ba61ade1beb5953e550178
Mrinmoy <minmoydholey0@gmail.com>
2025-11-11 12:33:23 +0530

    add the css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom"; // ✅ import navigate
-import "../Pages/login.css";
+import "./login.css";
 
 function Login() {
   const [logData, setLogData] = useState({ email: "", password: "" });
