084f70aeb7a69afdf740f0e6c38c739d630af049
Mrinmoy <minmoydholey0@gmail.com>
2025-11-11 12:38:55 +0530

    a

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom"; // ✅ import navigate
-import "./login.css";
+import "./Login.css";
 
 function Login() {
   const [logData, setLogData] = useState({ email: "", password: "" });
